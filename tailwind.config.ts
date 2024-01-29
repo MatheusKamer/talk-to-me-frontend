93f3e232d7cbc91473b6119ae608f8569079f5d5
Matheus Kamer <matheus_kamer@hotmail.com>
2024-01-29 15:44:53 -0400

    Finalizando a tela de acesso

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'gray-850': '#212121',
+        'gray-950': '#4E4E4E',
+        'gray-1000': '#2E2E2E',
+        'black-100': '#0F0F0F',
+        'ciano-100': '#81E6D9',
+      }
     },
   },
   plugins: [],
