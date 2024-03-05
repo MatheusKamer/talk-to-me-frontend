0258341d82c49ba10e34a4cc8efa540cc9fa893f
Matheus Kamer <matheus_kamer@hotmail.com>
2024-03-04 22:08:21 -0400

    feat: sending and rendering messages between users

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,44 +1,58 @@
-import { FormEvent, useContext, useEffect, useRef } from "react";
+import { FormEvent, useContext, useEffect, useRef, useState } from "react";
 import { SendMessage } from "@/Icons";
 import { SocketContext } from "@/contexts/SocketContext";
+
+interface iChatMessage {
+  message: string;
+  username: string;
+  roomId: string;
+  time: string;
+}
 
 export default function Chat({ roomId }: { roomId: string }) {
   const currentMessage = useRef<HTMLInputElement>(null)
   const { socket } = useContext( SocketContext )
+  const [chat, setChat] = useState<iChatMessage[]>([]);
 
   useEffect(() => {
     socket?.on('chat', (data) => {
       console.log('message: ', data)
+      setChat((prevState) => [...prevState, data])
     })
   },[socket])
 
   function sendMessage(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    if(currentMessage.current?.value !== '') {
+    if(currentMessage.current && currentMessage.current?.value !== '') {
       const sendMessageToServer = {
-        message: currentMessage.current?.value,
+        message: currentMessage.current.value,
         username: 'Matheus Kamer',
         roomId,
-        time: new Date().toLocaleTimeString
+        time: new Date().toLocaleTimeString(),
       };
 
       socket?.emit('chat', sendMessageToServer);
+      setChat((prevState) => [...prevState, sendMessageToServer]);
 
       currentMessage.current!.value = '';
     }
   }
 
   return (
-    <div className="md:flex flex-col justify-between h-full bg-gray-850 px-4 pt-4 w-[25%] hidden rounded-md m-3">
-      <div className="bg-gray-950 rounded p-2">
-        <div className="flex items-center space-x-2">
-          <span>Matheus Kamer</span>
-          <span>21:03</span>
+    <div className="md:flex flex-col justify-end h-full bg-gray-850 px-4 pt-4 w-[25%] hidden rounded-md m-3 overflow-auto">
+      <div >
+      {chat.map((chat, index) => (
+        <div className="bg-gray-950 rounded p-2 mb-4 " key={index}>
+          <div className="flex items-center space-x-2">
+            <span>{chat.username}</span>
+            <span>{chat.time}</span>
+          </div>
+          <div className="mt-4 text-sm">
+            <p>{chat.message}</p>
+          </div>
         </div>
-        <div className="mt-4 text-sm">
-          <p>text</p>
-        </div>
+      ))}
       </div>
 
       <form
@@ -51,7 +65,9 @@ export default function Chat({ roomId }: { roomId: string }) {
           ref={currentMessage}
           className="p-2 w-full rounded-sm bg-gray-750"
         />
-        <SendMessage className=" absolute w-4 h-4 right-2" />
+        <button className="absolute w-4 h-4 right-2" type="submit">
+          <SendMessage />
+        </button>
       </form>
     </div>
   );
