ee84980b2d299e596821c47d8da58055891b0001
Matheus Kamer <matheus_kamer@hotmail.com>
2024-03-09 20:16:58 -0400

    feat: add function to start camera

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -4,20 +4,37 @@ import Chat from "@/components/Chat";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { SocketContext } from "@/contexts/SocketContext";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 
 export default function Room({ params }: { params: { id: string } }) {
   const { socket } = useContext(SocketContext);
+  const localStream = useRef<HTMLVideoElement>(null)
+  const username = sessionStorage.getItem('username')
 
   useEffect(() => {
-    socket?.on('connect', () => {
+    socket?.on('connect', async () => {
       console.log('Conectado');
       socket.emit('subscribe', {
         roomId: params.id,
         socketId: socket.id
       })
+      await handleStartCamera()
     })
   },[socket, params.id])
+
+  const handleStartCamera = async () => {
+    const video = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: {
+        noiseSuppression: true,
+        echoCancellation: true,
+      }
+    })
+
+    if (localStream.current) {
+      localStream.current.srcObject = video;
+    }
+  }
 
   return (
     <div className="h-screen">
@@ -26,8 +43,8 @@ export default function Room({ params }: { params: { id: string } }) {
         <div className="md:w-[75%] w-full mt-3 mx-3">
           <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
             <div className="bg-gray-950 w-full rounded-md h-full gap-4 p-2 relative">
-              <video className="h-full w-full"></video>
-              <span className="absolute bottom-3">Matheus Kamer</span>
+              <video className="h-full w-full" autoPlay playsInline ref={localStream}/>
+              <span className="absolute bottom-3">{username}</span>
             </div>
             <div className="bg-gray-950 w-full rounded-md h-full gap-4 p-2 relative">
               <video className="h-full w-full"></video>
